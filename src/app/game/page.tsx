19ab699c11a93d746cafa5a0c90cd9d59542ba70
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-11-17 00:20:46 +0530

    Refactor: Adjust padding in GamePage and add creator attribution

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -8,7 +8,7 @@ export default function GamePage() {
       <header className="bg-gray-800 py-3 px-4 lg:py-4 lg:px-6 text-center">
         <h1 className="text-2xl lg:text-3xl font-bold">Chess P2P</h1>
       </header>
-      <main className="flex-grow flex items-center justify-center p-2 sm:p-4 lg:p-6">
+      <main className="flex-grow flex items-center justify-center p-2 sm:p-4 lg:p-[1rem]">
         <div className="bg-gray-800 border border-gray-700 rounded-lg overflow-auto w-full max-w-[90vw] max-h-[100vh] lg:overflow-hidden lg:transform lg:scale-90 lg:origin-top">
           <div className="grid grid-cols-1 lg:grid-cols-[1.2fr,2.1fr,1fr] gap-3 lg:gap-4 p-4 lg:p-6 h-full">
             <VideoCall />
@@ -19,6 +19,9 @@ export default function GamePage() {
           </div>
         </div>
       </main>
+      <span className="text-center text-gray-400 block py-4 lg:pt-[1px]">
+          Made with ❤️ by <a href="https://x.com/JatinSriva36542?t=jInLA9mjPJc3klWMBmAhDg&s=09" className="underline">Jatin</a>
+      </span>
     </div>
   );
 }
