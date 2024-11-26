2d6f85c2ee51eaafc968e63b0e04619345281a5b
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-11-26 22:06:19 +0530

    feat: Add TURN server configuration file for WebRTC support

---
+++ src/config/turnServerConfig.ts
@@ -0,0 +1,22 @@
+export const turnServerConfig = [
+  {
+    urls: "turn:global.relay.metered.ca:80",
+    username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME,
+    credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIAL,
+  },
+  {
+    urls: "turn:global.relay.metered.ca:80?transport=tcp",
+    username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME,
+    credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIAL,
+  },
+  {
+    urls: "turn:global.relay.metered.ca:443",
+    username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME,
+    credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIAL,
+  },
+  {
+    urls: "turns:global.relay.metered.ca:443?transport=tcp",
+    username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME,
+    credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIAL,
+  },
+];
